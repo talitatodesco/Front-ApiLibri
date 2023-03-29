ff9dfefc12390b288f0d3c7c1b126cbc665074b4
Talita Marcondes <70537487+talitatodesco@users.noreply.github.com>
2023-03-29 00:33:43 -0300

    Subindo

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const axios = require('axios').default;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+/* INICIO DAS CONFIGURAÇÕES DO EJS:  */
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+/* FIM DAS CONFIGURAÇÕES DO EJS:  */
+
+/* INICIO DAS ROTAS DE ACESSO AS PÁGINAS EJS*/
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+/* FIM DAS ROTAS DE ACESSO AS PÁGINAS EJS*/
+
+/* INICIO DAS ROTAS DE CATEGORIA */
+
+/*CADASTRO*/
+app.get('/categoria', (req, res)=>{
+    res.render('categoria/index');
+});
+
+/*LISTAGEM*/
+app.get('/listagemCategorias', (req, res)=>{
+   
+    /* CONFIGURAÇÃO DA REQUISIÇÃO BACK END VIA AXIOS*/
+
+    /* ROTA DO BACK END */
+    const urlListarCategoria = 'http://localhost:3000/listarCategoria';
+
+    /*
+     CHAMADA DO AXIOS PARA A ROTA DO BACK END 
+     PARAMETROS DO VERBO:
+     1 - ROTA
+     2 - .then DE TRATAMENTO DA RESPOSTA
+     */
+    axios.get(urlListarCategoria)
+    .then((response)=>{
+
+        console.log(response.data);
+        let categorias = response.data;
+        res.render('categoria/listagemCategoria', {categorias});
+
+    });
+});
+
+/*EDITAR*/
+app.get('/editarCategoria/:cod_categoria', (req, res)=>{
+
+    let {cod_categoria} = req.params;
+
+    urlListarCategoriaPK = `http://localhost:3000/listarCategoriaPK/${cod_categoria}`;
+
+    // console.log("COD_CATEGORIA: " + cod_categoria); 
+
+    axios.get(urlListarCategoriaPK)
+        .then((response)=>{
+            let categoria = response.data;
+            //console.log(categoria.data);
+            res.render('categoria/editarCategoria.ejs', {categoria});
+
+        });
+
+});
+
+app.post('/editarCategoria', (req, res)=>{
+
+    //console.log(req.body);
+
+    let urlEditar = 'http://localhost:3000/alterarCategoria';
+
+    axios.put(urlEditar, req.body)
+        .then((response)=>{
+            res.send('DADO ALTERADO');
+        });
+
+});
+
+/* FIM DAS ROTAS DE CATEGORIA */
+
+app.listen(3001, ()=>{
+    console.log("SERVIDOR FRONTEND RODANDO EM - http://localhost:3001");
+});
